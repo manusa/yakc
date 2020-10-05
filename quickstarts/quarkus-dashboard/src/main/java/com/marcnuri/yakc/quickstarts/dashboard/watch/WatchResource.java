06ad79bfb39c4eaefbf29ef9bc2dc992d3e47bc9
Marc Nuri <marc@marcnuri.com>
2020-10-05 20:35:38 +0200

    [Dashboard] fix: Missing change in throws signature

--- quickstarts/quarkus-dashboard/src/main/java/com/marcnuri/yakc/quickstarts/dashboard/watch/WatchResource.java
+++ quickstarts/quarkus-dashboard/src/main/java/com/marcnuri/yakc/quickstarts/dashboard/watch/WatchResource.java
@@ -1,17 +1,17 @@
 package com.marcnuri.yakc.quickstarts.dashboard.watch;
 
-import com.marcnuri.yakc.api.KubernetesException;
 import com.marcnuri.yakc.api.WatchEvent;
 import com.marcnuri.yakc.model.Model;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.converters.multi.MultiRxConverters;
-import org.jboss.resteasy.annotations.SseElementType;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.jboss.resteasy.annotations.SseElementType;
+
+import java.io.IOException;
 
 @Singleton
 public class WatchResource {
@@ -26,7 +26,7 @@ public class WatchResource {
   @GET
   @Produces(MediaType.SERVER_SENT_EVENTS)
   @SseElementType(MediaType.APPLICATION_JSON)
-  public Multi<WatchEvent<? extends Model>> get() throws KubernetesException {
+  public Multi<WatchEvent<? extends Model>> get() throws IOException {
     return Multi.createFrom().converter(MultiRxConverters.fromObservable(),
       watchService.getWatch());
   }
