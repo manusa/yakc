fab32602c2fca69cc232a24dedeb0d2170def825
Marc Nuri <marc@marcnuri.com>
2020-11-28 15:58:36 +0100

    [Dashboard] refactor: Use Jackson instead of jsonb, reuse YAKC (de)serializers

---
+++ quickstarts/quarkus-dashboard/src/main/java/com/marcnuri/yakc/quickstarts/dashboard/JacksonConfiguration.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 Marc Nuri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created on 2020-11-28, 12:41
+ */
+package com.marcnuri.yakc.quickstarts.dashboard;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import io.quarkus.jackson.ObjectMapperCustomizer;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class JacksonConfiguration  implements ObjectMapperCustomizer {
+
+  public void customize(ObjectMapper mapper) {
+    mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  }
+}
