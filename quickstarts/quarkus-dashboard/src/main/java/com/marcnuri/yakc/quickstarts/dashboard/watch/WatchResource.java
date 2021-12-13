ceea6654755fe0826506e7835e9644d832fa6bc2
Marc Nuri <marc@marcnuri.com>
2021-12-13 11:51:56 +0100

    [Dashboard] Prevent Deadlocks Vert.x + Mutiny
    https://groups.google.com/g/Quarkus-dev/c/t5r6-VCrRMw?pli=1
    
    > That thread should never be use to do blocking IO, as it is the event loop thread. The way blocking IO is implemented it actually needs the event loop thread to notify the thread to unblock, so if the event loop thread is blocked you deadlock.
    
    Final Watch Observable to SSE pipe subscription must happen on a separate thread.

--- quickstarts/quarkus-dashboard/src/main/java/com/marcnuri/yakc/quickstarts/dashboard/watch/WatchResource.java
+++ quickstarts/quarkus-dashboard/src/main/java/com/marcnuri/yakc/quickstarts/dashboard/watch/WatchResource.java
@@ -18,11 +18,15 @@
 package com.marcnuri.yakc.quickstarts.dashboard.watch;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
 import io.quarkus.runtime.annotations.RegisterForReflection;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
 import org.jboss.resteasy.annotations.SseElementType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
@@ -31,6 +35,8 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.sse.Sse;
 import javax.ws.rs.sse.SseEventSink;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Singleton
 @RegisterForReflection // Quarkus doesn't generate constructors for JAX-RS Subresources
@@ -40,20 +46,28 @@ public class WatchResource {
 
   private final WatchService watchService;
   private final ObjectMapper objectMapper;
+  private final ExecutorService subscribeExecutor;
 
   @Inject
   public WatchResource(WatchService watchService, ObjectMapper objectMapper) {
     this.watchService = watchService;
     this.objectMapper = objectMapper;
+    subscribeExecutor = Executors.newCachedThreadPool();
+  }
+
+  void onShutdown(@Observes ShutdownEvent event) {
+    subscribeExecutor.shutdown();
   }
 
   @GET
   @Produces(MediaType.SERVER_SENT_EVENTS)
   @SseElementType(MediaType.APPLICATION_JSON)
   public void get(@Context Sse sse, @Context SseEventSink sseEventSink) {
-    watchService.newWatch().subscribe().with(we -> {
+    watchService.newWatch().runSubscriptionOn(subscribeExecutor).subscribe().with(we -> {
       try {
-        sseEventSink.send(sse.newEvent(objectMapper.writeValueAsString(we)));
+        if (!sseEventSink.isClosed()) {
+          sseEventSink.send(sse.newEvent(objectMapper.writeValueAsString(we)));
+        }
       } catch (Exception e) {
         LOG.error("Error serializing object", e);
       }
--- quickstarts/quarkus-dashboard/src/main/java/com/marcnuri/yakc/quickstarts/dashboard/watch/Watchable.java
+++ quickstarts/quarkus-dashboard/src/main/java/com/marcnuri/yakc/quickstarts/dashboard/watch/Watchable.java
@@ -30,14 +30,6 @@ import java.util.Optional;
 
 public interface Watchable<T extends Model> {
 
-  /**
-   * TODO: Document better
-   *
-   * <p> If an Empty Optional is returned, the subscription will not be retried.
-   *
-   * @return an Optional of an Observable WatchEvent
-   * @throws IOException if there are IO problems when opening the watch
-   */
   Observable<WatchEvent<T>> watch() throws IOException;
 
   default String getType() {
